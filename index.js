ee0330d82b0cbd41fd83d49f873217e6c07a6674
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2025-09-17 12:55:36 +0300

    feat: Added a new table ville

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ app.use(express.json());
 const userRouter = require('./src/utilisateur/user');
 const reservationRouter = require('./src/reservation');
 const trainRouter = require('./src/train');
+const villeRouter = require('./src/vile');
 const trajetRouter = require('./src/trajet');
 //Routing the endpoint
 app.use('/user' , userRouter)
 app.use('/reservation', reservationRouter);
 app.use('/train', trainRouter);
 app.use('/trajet', trajetRouter);
+app.use('/ville', villeRouter);
 //Listening to port
 app.listen(port , () => {
     console.log(`The app listening on ${port}`);
---
+++ src/vile.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
+
+// Database connection
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "reservation_train",
+});
+
+// Get all ville
+router.get("/all", (req, res) => {
+    const SELECT_ALL_VILLE_QUERY = "SELECT * FROM ville";
+
+    db.query(
+        SELECT_ALL_VILLE_QUERY,
+        (err, data) => {
+            if (err) {
+                console.log(err);
+                return res.json(err);
+            }
+            return res.json(data);
+        }
+    );
+});
+
+// Get ville by id
+router.get("/get/:id", (req, res) => {
+    const id = req.params.id;
+    const SELECT_VILLE_BY_ID_QUERY = "SELECT * FROM ville WHERE code_ville = ?";
+
+    db.query(
+        SELECT_VILLE_BY_ID_QUERY,
+        [id],
+        (err, data) => {
+            if (err) {
+                console.log(err);
+                return res.json(err);
+            }
+            return res.json(data);
+        }
+    );
+});
+
+// Add a new ville
+router.post("/create", (req, res) => {
+    const { code_ville, nom_ville, photo_ville } = req.body;
+
+    const INSERT_VILLE_QUERY = "INSERT INTO ville (code_ville, nom_ville, photo_ville) VALUES (? , ?, ?)";
+
+    db.query(
+        INSERT_VILLE_QUERY,
+        [code_ville, nom_ville, photo_ville],
+        (err, data) => {
+            if (err) {
+                console.log(err);
+                return res.json(err);
+            }
+            return res.json(data);
+        }
+    );
+});
+
+// Delete a ville
+router.delete("/delete/:id", (req, res) => {
+    const id = req.params.id;
+
+    const DELETE_VILLE_QUERY = "DELETE FROM ville WHERE code_ville = ?";
+
+    db.query(
+        DELETE_VILLE_QUERY,
+        [id],
+        (err, data) => {
+            if (err) {
+                console.log(err);
+                return res.json(err);
+            }
+            return res.json(data);
+        }
+    );
+});
+
+// Update a ville
+router.patch("/edit/:id", (req, res) => {
+    const id = req.params.id;
+    const { nom_ville, photo_ville } = req.body;
+
+    const UPDATE_VILLE_QUERY = "UPDATE ville SET nom_ville = ? , photo_ville = ? WHERE code_ville = ?";
+
+    db.query(
+        UPDATE_VILLE_QUERY,
+        [nom_ville, photo_ville, id],
+        (err, data) => {
+            if (err) {
+                console.log(err);
+                return res.json(err);
+            }
+            return res.json(data);
+        }
+    );
+});
+
+module.exports = router;
